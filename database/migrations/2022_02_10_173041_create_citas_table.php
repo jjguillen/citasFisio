5a00a3ce5108064225e375aed8afd15dd1e470e4
jjguillen <jjavierguillen@gmail.com>
2022-02-10 18:58:29 +0100

    first commit

---
+++ database/migrations/2022_02_10_173041_create_citas_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('citas', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->date('fecha');
+            $table->integer('hora');
+            $table->text('observaciones');
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('servicio_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('servicio_id')->references('id')->on('servicios');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('citas');
+    }
+};
